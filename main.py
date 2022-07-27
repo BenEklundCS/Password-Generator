06e78f29c489a38a2da4923fe33559361c4c3f41
Ben Eklund <>
2022-07-27 14:55:39 +0000

    Split functions into password.py file, added a 50/50 chance to caps characters. Next - figure out OOP approach to the same program.

--- main.py
+++ main.py
@@ -1,46 +1,8 @@
 # Password Generator
 # Benjamin Eklund
 
-import random, time
+from password import *
 
-def getInput():
-  # Add more choices like optional inclusion of special characters
-  print("Enter desired password length: ")
-  passwordLength = input()
-  try:
-    int(passwordLength)
-    it_is = True
-  except ValueError:
-    it_is = False
-    print('Input was not accepted - "ENTER AN INTEGER"')
-    
-  return passwordLength
-
-def generateChar():
-
-  chars = ['a','b','c','d','e',
-           'f','g','h','i','j',
-           'k','l','m','n','o',
-           'p','q','r','s','t',
-           'u','v','w','x','y',
-           'z','!','@','#','$',
-           '%','^','&','*']
-  
-  random.seed(time.time())
-  roll = random.randrange(0,34)
-  char = chars[roll]
-  # add functionality to randomly uppercase letters 50% of the time
-  return char
-  
-def generatePassword(length):
-  password = []
-  for i in range(int(length)):
-    char = generateChar()
-    password.append(char)
-  str(password)
-  result = (''.join(map(str, password)))
-  return result
-  
 def main():
   
   length = getInput()
